2b3f659773cb2670799dac2c4e4488fe0ed91938
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-04-02 19:07:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -26,12 +26,12 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Mo_Tech_YT</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @MRK_YT
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
